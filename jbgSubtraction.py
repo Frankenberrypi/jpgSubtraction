4477ef91ccd455d514eac62bb475de53d3b5f300
Jeffrey Beckman <JeffreyJBeckman@gmail.com>
2014-11-13 19:52:15 -0800

    Initial commit

---
+++ jbgSubtraction.py
@@ -0,0 +1,9 @@
+from scipy import misc
+import numpy as np
+
+before = misc.imread('before.JPG',flatten=1)
+after = misc.imread('after.JPG',flatten=1)
+
+subtracted = np.subtract(before,after)
+
+misc.imsave('subtracted.jpg',subtracted)
