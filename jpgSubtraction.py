e98a19cfe10b3c91de4dbadbf96d7f2c4551589c
unknown <jbeckman@Jacqueline.quartus.com>
2017-05-18 10:25:33 -0700

    Generalize for different file names, user input required.

--- jpgSubtraction.py
+++ jpgSubtraction.py
@@ -1,9 +1,23 @@
-#
+# for Python 3.6
+# known to work on .jpg and .tif files
+# probably works on .png and .gif files
+# no workie on .pdf files
+
 from scipy import misc
 import numpy as np
 
-before = misc.imread('before.JPG',flatten=1)
-after = misc.imread('after.JPG',flatten=1)
+# Get the files
+print("Enter first file name")
+beforeFile = input()
+print("Enter second file name")
+afterFile = input()
+
+
+# Read files in
+#before = misc.imread('Seattle TAC 87.tif',flatten=1)
+#after = misc.imread('Seattle TAC 88.tif',flatten=1)
+before = misc.imread(beforeFile,flatten=1)
+after = misc.imread(afterFile,flatten=1)
 
 subtracted = np.subtract(before,after)
 
